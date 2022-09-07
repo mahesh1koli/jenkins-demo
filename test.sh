4a172032d3f8910ab64b5960cab344aea16d207c
Mahesh Kolisetti <52033735+mahesh1koli@users.noreply.github.com>
2022-09-07 13:57:55 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-mkdir -p /home/ubuntu/demo
-chmod 777 /home/ubuntu/demo
-touch /home/ubuntu/demo/file1.txt
+mkdir -p /home/ubuntu/demo/sample
+chmod 777 /home/ubuntu/demo/sample
+touch /home/ubuntu/demo/sample/file1.txt
