546be3e4993634fd136ec2a9f12558a3bcf2ab48
Mahesh Kolisetti <52033735+mahesh1koli@users.noreply.github.com>
2022-09-07 13:44:07 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-mkdir -p /home/ec2-user/demo
-chmod 777 /home/ec2-user/demo
-touch /home/ec2-user/demo/file1.txt
+mkdir -p /home/ubuntu/demo
+chmod 777 /home/ubuntu/demo
+touch /home/ubuntu/demo/file1.txt
